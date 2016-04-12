9f2573f5ac61e316f1ba43df3c390f7006178ee0
swk <swkeemink@scimail.eu>
2016-04-12 13:10:25 +0100

    Initialized repo

---
+++ install_scratch.sh
@@ -0,0 +1,9 @@
+sudo ln -s /usr/lib/i386-linux-gnu/libgnome-keyring.so.0 /usr/lib/libgnome-keyring.so.0
+
+sudo ln -s /usr/lib/i386-linux-gnu/libgnome-keyring.so.0.2.0 /usr/lib/libgnome-keyring.so.0.2.0
+
+chmod +x ./AdobeAIRInstaller.bin
+
+sudo ./AdobeAIRInstaller.bin
+
+sudo "Adobe AIR Application Installer" /home/gameon/scratch/Scratch-444.4.air
---
+++ install_scratch_64.sh
@@ -0,0 +1,11 @@
+sudo apt-get install libxt6:i386 libnspr4-0d:i386 libgtk2.0-0:i386 libstdc++6:i386 libnss3-1d:i386 lib32nss-mdns libxml2:i386 libxslt1.1:i386 libcanberra-gtk-module:i386 gtk2-engines-murrine:i386
+
+sudo ln -s /usr/lib/x86_64-linux-gnu/libgnome-keyring.so.0 /usr/lib/libgnome-keyring.so.0
+
+sudo ln -s /usr/lib/x86_64-linux-gnu/libgnome-keyring.so.0.2.0 /usr/lib/libgnome-keyring.so.0.2.0
+
+chmod +x ./AdobeAIRInstaller.bin
+
+sudo ./AdobeAIRInstaller.bin
+
+sudo "Adobe AIR Application Installer" /home/gameon/scratch/Scratch-444.4.air
